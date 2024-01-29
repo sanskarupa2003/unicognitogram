9704b8cf1f3327357494ddd139ba8a9c32e7cfda
SANSKAR UPADHYAY <sanskarupadhyay9@gmail.com>
2024-01-29 14:43:31 +0530

    added react app with landing page

---
+++ reactfirst/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logow.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container" style={{ backgroundColor: '#000504' }}>
+    <div className="text">
+   
+    <h3 style={{ color: '#F53816', fontFamily: 'Praise' }}>You have gone Unicognito</h3>
+    <h1 style={{ color: '#FFFFFF', fontFamily: 'Playfair Display' }}>UNICOGNITO</h1>
+    </div>
+    <div className="App-header">
+      
+        <img src={logo} className="App-logo" alt="logo" />
+      
+    </div>
+    </div>
+  );
+}
+
+export default App;
