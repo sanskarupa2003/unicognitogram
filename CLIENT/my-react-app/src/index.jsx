0bfc4864bfa43558586d8bf4f8dbfa1e5706c961
SANSKAR UPADHYAY <sanskarupadhyay9@gmail.com>
2024-02-12 02:06:52 +0530

    Vite Build Done

--- CLIENT/my-react-app/src/index.jsx
+++ CLIENT/my-react-app/src/index.jsx
@@ -1,36 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Signup from './Signup';
 import Signin from './Signin';
-import Feed from './feed'
+import Feed from './Feed';
 import reportWebVitals from './reportWebVitals';
-
-
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-  
   <BrowserRouter>
-  <Routes>
-    <Route path="/home" element={<App />}/>
-    <Route path="/home/signup" element={<Signup/>}/>
-    <Route path="/home/signin" element={<Signin/>}/>
-    <Route path="/home/feed" element={<Feed/>}/>
-    <Route path="/*" element={<h1 style={{ color: '#FFFFFF'}}>Go to /home</h1> }/>
-    
-  </Routes>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/signin" component={Signin} />
+      <Route path="/feed" component={Feed} />
+      <Route path="/*" render={() => <h1 style={{ color: '#FFFFFF' }}>Go to /</h1>} />
+    </Switch>
   </BrowserRouter>
-  </>
-
 );
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
